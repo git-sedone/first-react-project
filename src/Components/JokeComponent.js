83efd8003d7292eee62d99f3f391c56e18b21165
Kuily <kuily@Niroshans-MacBook-Pro.local>
2021-09-25 18:31:08 +0100

    added twittezer api for fetching joke

--- src/Components/JokeComponent.js
+++ src/Components/JokeComponent.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './JokeComponent.css';
 
-
 class JokeComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
-      jokeText: {},
+      jokeText: '',
+      jokePunchLine: '',
       showAnswerButton: false,
       showSolution: false,
-      
     };
     this.randomJokeHandler = this.randomJokeHandler.bind(this);
     this.changeHandler = this.changeHandler.bind(this);
@@ -29,69 +27,69 @@ class JokeComponent extends React.Component {
     this.setState({
       answer: event.target.value,
     });
-    console.log(event.target.value);
   }
 
   randomJokeHandler(event) {
-    console.log('click');
-    fetch('https://official-joke-api.appspot.com/random_joke')
+    fetch('https://twittezer.herokuapp.com/')
       .then((res) => res.json())
-      .then((data) =>
+      .then((responseData) => {
+        const { data } = responseData;
+        const randomJokeObject = data[Math.floor(Math.random() * data.length)];
+
         this.setState({
-          jokeText: data,
+          jokeText: randomJokeObject.joke_text,
+          jokePunchLine: randomJokeObject.joke_punchline,
           showAnswerButton: true,
           showSolution: ' ',
-        })
-      );
+        });
+      });
   }
-  openJoke(){
+  openJoke() {
     this.setState({
-      showJoke: true
-    })
+      showJoke: true,
+    });
   }
-  closeJoke(){
+  closeJoke() {
     this.setState({
-      showJoke:false,
-      jokeText:'',
+      showJoke: false,
+      jokeText: '',
       showAnswerButton: false,
-      showSolution:''
-
-
-    })
-
+      showSolution: '',
+    });
   }
 
   render() {
-    
-
     return (
       <div>
-        <div className='hamburger-menu'>
-            <i className="fa fa-bars fa-3x" id="bars" onClick={this.openJoke}></i>
+        <div className="hamburger-menu">
+          <i className="fa fa-bars fa-3x" id="bars" onClick={this.openJoke}></i>
         </div>
-        <div className='joke-btn' onClick={this.openJoke}>Smile with Me</div>
-        {this.state.showJoke ?
-        
-        <div className="joke-container">
-          <i className="fa fa-times fa-3x" onClick={this.closeJoke}></i>
-          <h1>Start the day with a Laugh</h1>
-          <button className='get-joke-btn' onClick={this.randomJokeHandler}>Get Joke</button>
-          <h1>{this.state.jokeText.setup}</h1>{' '}
-          {this.state.showAnswerButton === true ? (
-            <div className='punch-container'>
-              <button className='get-punch-btn' onClick={this.answerClickHandler}>check Punchline</button>
-            
-            </div>
-          ) : null}
-          {this.state.showSolution === true ? (
-            <h1>{this.state.jokeText.punchline}</h1>
-          ) : null}
+        <div className="joke-btn" onClick={this.openJoke}>
+          Smile with Me
         </div>
-        : null
-      }
-        
-        
-    
+        {this.state.showJoke ? (
+          <div className="joke-container">
+            <i className="fa fa-times fa-3x" onClick={this.closeJoke}></i>
+            <h1>Start the day with a Laugh</h1>
+            <button className="get-joke-btn" onClick={this.randomJokeHandler}>
+              Get Joke
+            </button>
+            <h1>{this.state.jokeText}</h1>{' '}
+            {this.state.showAnswerButton === true ? (
+              <div className="punch-container">
+                <button
+                  className="get-punch-btn"
+                  onClick={this.answerClickHandler}
+                >
+                  check Punchline
+                </button>
+              </div>
+            ) : null}
+            {this.state.showSolution === true ? (
+              <h1>{this.state.jokePunchLine}</h1>
+            ) : null}
+          </div>
+        ) : null}
       </div>
     );
   }
