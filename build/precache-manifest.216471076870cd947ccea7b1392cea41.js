58ef4ff1fe472ec2b11eed2471433c59ab6e7403
niroshan nadarajah <nnadarajah@coolbet.com>
2023-01-01 17:45:54 +0000

    modified lock and created build directory

---
+++ build/precache-manifest.216471076870cd947ccea7b1392cea41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0422f98764c230714f9d0ba8bec1f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e25b87f069b69a259b4e",
+    "url": "/static/css/main.f191aeff.chunk.css"
+  },
+  {
+    "revision": "79b6189158460f4f95c6",
+    "url": "/static/js/2.d894594f.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.d894594f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25b87f069b69a259b4e",
+    "url": "/static/js/main.bb4205db.chunk.js"
+  },
+  {
+    "revision": "78adc99a024084a9b424",
+    "url": "/static/js/runtime-main.274ecbbe.js"
+  },
+  {
+    "revision": "4b440c2c5b5ca4072e469ce12b8043cf",
+    "url": "/static/media/smile.4b440c2c.jpg"
+  }
+]);
